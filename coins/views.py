678d9f32ac0e87bed94c5de673701b0319cedbf9
Diana Salsbury <diana.salsbury@gmail.com>
2013-12-24 16:17:19 -0500

    adding image hooks for detail page

--- coins/views.py
+++ coins/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from coins.models import Coin
+from coins.models import Coin, Image
 
 # Create your views here.
 def index(request):
     recently_added_coins_list = Coin.objects.order_by('-pub_date')[:5]
+    images_list = Image.objects.all()
     template = loader.get_template('coins/index.html')
-    context = {'recently_added_coins_list': recently_added_coins_list}
+    context = {'recently_added_coins_list': recently_added_coins_list,
+               'images_list': images_list}
     return render(request, 'coins/index.html', context)
 
 def view_collection(request):
@@ -19,4 +21,8 @@ def view_collection(request):
 
 def coin(request, coin_id):
     coin = get_object_or_404(Coin, id=coin_id)
-    return render(request, 'coins/detail.html', {'coin': coin})
+    image = Image.objects.get(coin=coin)
+    context = {'coin': coin,
+               'image': image,
+               }
+    return render(request, 'coins/detail.html', context)
